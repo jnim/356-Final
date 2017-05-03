03ee345f92bb7f2669cc1a18f0b3dfc7d57e9585
jnimgaonka <jnimgaonka@haverford.edu>
2017-05-03 12:07:29 -0400

    going to look at Julia's code before I actually work on Bitonic Sort

--- src/Bitonicsort/SequentialBitonic.java
+++ src/Bitonicsort/SequentialBitonic.java
@@ -1,5 +1,28 @@
 package Bitonicsort;
 
 public class SequentialBitonic {
+	//general idea: for each run, you sort through the number of times you have to sort
+	/*
+	 * Sort up(4)
+	 * 	Sort up(2)
+	 * 		Sort up(1)
+	 * 		Sort down(1)
+	 * 	Sort down(2)
+	 * 		Sort up(1)
+	 * 		Sort down(1)
+	 * 
+	 * Sort down(4)
+	 * Swap(inc = size/2)
+	 * 	Swap(inc/2
+	
+	*/
+	public void run(int a[]){
+		
+	}
+	
+	int swap(int a[], int increment){
+		return 0; 
+	}
+
 
 }
